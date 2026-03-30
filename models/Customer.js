43ea997795829117a2a60bd496d09813bf9aed08
desireeamber-sudo <desireeamber@gmail.com>
2026-03-29 21:55:14 -0500

    Add customer model and routes

---
+++ models/Customer.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  phone: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const Customer = require("./models/Customer");
 
 const app = express();
 
@@ -10,6 +11,34 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req, res) => {
     res.send("YogiTrack server is running");
+});
+
+app.get("/add-test-customer", async (req, res) => {
+  try {
+    const customer = new Customer({
+      firstName: "Test",
+      lastName: "User",
+      email: "test@example.com",
+      phone: "555-1234"
+    });
+
+    await customer.save();
+
+    res.send("Test customer added");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding customer");
+  }
+});
+
+app.get("/customers", async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving customers");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
