29855d30e33b218b7380ed154c0fc3d5e6c7b7b4
desireeamber-sudo <desireeamber@gmail.com>
2026-03-29 21:41:21 -0500

    Connect app to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error(err));
 
 app.get("/", (req, res) => {
     res.send("YogiTrack server is running");
