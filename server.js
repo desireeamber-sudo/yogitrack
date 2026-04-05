f3e0a7847153b4425950a90c9c111bc34370029d
desireeamber-sudo <desireeamber@gmail.com>
2026-04-04 21:20:25 -0500

    Serve static HTML files from Express

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
